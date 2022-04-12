009c9a5f43a633025c614f1636891912519c33d1
elvis <elvisduru76@gmail.com>
2022-04-12 13:36:15 +0100

    todo

--- todo/settings.py
+++ todo/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
